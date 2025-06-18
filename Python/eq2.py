cf3749fd0b34bd807e731b4a9227b425fe6fcf3e
Rafael Matos <rafael.matos2.rm@gmail.com>
2025-06-17 23:40:08 -0300

    Comando mais bonitinho

--- Python/eq2.py
+++ Python/eq2.py
@@ -7,16 +7,16 @@ if a == 0:
     print ('A = 0, portanto, não é uma equação de segundo grau!')
     exit()
 elif delta >= 0:
-    bhask1 = (-b + sqrt(delta)) / (2*a)
-    bhask2 = (-b - sqrt(delta)) / (2*a)
-    if bhask1.is_integer():
-        bhask1_formatado = int(bhask1)
+    x1 = (-b + sqrt(delta)) / (2*a)
+    x2 = (-b - sqrt(delta)) / (2*a)
+    if x1.is_integer():
+        x1_f = int(x1)
     else: 
-        bhask1_formatado = bhask1
-    if bhask2.is_integer():
-        bhask2_formatado = int(bhask2)
+        x1_f = x1
+    if x2.is_integer():
+        x2_f = int(x2)
     else:
-        bhask2_formatado = bhask2
+        x2_f = x2
     if a.is_integer():
         a1 = int(a)
     else:
@@ -29,6 +29,6 @@ elif delta >= 0:
         c1 = int(c)
     else:
         c1 = c
-    print ('A equação {}x² + {}x + {} tem as raízes:\n{}\n{}'.format(a1, b1, c1, bhask1_formatado, bhask2_formatado))
+    print ('A equação {}x² + {}x + {} tem as raízes:\n{}\n{}'.format(a1, b1, c1, x1_f, x2_f))
 else:
     print ('Delta é menor de zero. Portanto, não tem raízes reais.')
